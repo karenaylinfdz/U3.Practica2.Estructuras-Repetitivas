3065b68c96c0f8cb606a213c7065d980f7d1ac3a
karenaylinfdz <karenaylinfdz45@gmail.com>
2025-10-30 10:37:23 -0500

    Add files via upload

---
+++ U3-2 Practica 11/WinFormsApp22/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WinFormsApp22
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.IndianRed;
+            button1.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button1.Location = new Point(638, 177);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 37);
+            button1.TabIndex = 0;
+            button1.Text = "for";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Brown;
+            button2.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button2.Location = new Point(638, 252);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 34);
+            button2.TabIndex = 1;
+            button2.Text = "while";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LightCoral;
+            button3.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button3.Location = new Point(638, 327);
+            button3.Name = "button3";
+            button3.Size = new Size(88, 32);
+            button3.TabIndex = 2;
+            button3.Text = "do..while";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Red;
+            button4.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button4.Location = new Point(638, 391);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 30);
+            button4.TabIndex = 3;
+            button4.Text = "Salir";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(392, 222);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 199);
+            listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(203, 222);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(120, 199);
+            listBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(309, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 24);
+            label1.TabIndex = 6;
+            label1.Text = "f(x)= ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label2.Location = new Point(253, 164);
+            label2.Name = "label2";
+            label2.Size = new Size(23, 22);
+            label2.TabIndex = 7;
+            label2.Text = "X";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label3.Location = new Point(439, 164);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 22);
+            label3.TabIndex = 8;
+            label3.Text = "f(x)";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(422, 30);
+            label4.Name = "label4";
+            label4.Size = new Size(23, 22);
+            label4.TabIndex = 9;
+            label4.Text = "X";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(365, 47);
+            label5.Name = "label5";
+            label5.Size = new Size(136, 22);
+            label5.TabIndex = 10;
+            label5.Text = "---------------------";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(401, 80);
+            label6.Name = "label6";
+            label6.Size = new Size(67, 22);
+            label6.TabIndex = 11;
+            label6.Text = "1+X^2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "X";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
